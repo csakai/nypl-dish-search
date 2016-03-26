3278e136e54d1ccfbadd916355c358c6b34b955f
Chris Sakai <cs.chris.sakai@gmail.com>
2016-03-26 14:05:15 -0400

    Add config package and a default configuration. Apply this chnage to the server/index.js

--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
 /*jshint node:true*/
 'use strict';
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 8000;
-var routes;
+var config = require('config'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port,
+    routes;
+
+if (process.env.PORT) {
+    port = process.env.PORT;
+} else if (config.has('PORT')) {
+    port = config.get('PORT');
+} else {
+    port = 8000;
+}
+
+if (!config.has('API_KEY') || config.get('API_KEY') === 'YOUR_API_KEY') {
+    console.log('Please specify a real API_KEY. This can be easily accomplished by adding a new config file to the config folder,');
+    console.log('named something like "default-csakai.json", with an API_KEY specified.');
+    process.exit(1);
+}
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 routes = require('./routes');
 
-var source = '';
-
 app.get('/ping', function(req, res, next) {
-    console.log(req.body);
+    console.log(req.query);
     res.send('pong');
 });
 
