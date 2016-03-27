06882aa13651a2e4916335371cf10cdee0d20051
Chris Sakai <cs.chris.sakai@gmail.com>
2016-03-26 21:23:05 -0400

    Add oldest, newest, most popular callbacks to dish search. TODO: implement rate limiting since the API only accepts 2 requests per second

--- server/api/dishes/dish.controller.js
+++ server/api/dishes/dish.controller.js
@@ -1,14 +1,47 @@
-var config = require('config'),
+var _ = require('lodash'),
     Bluebird = require('bluebird'),
     requestBuilder = require('../../util/request_builder');
 
 function DishCtrl() {
-    this.API_KEY = config.get('API_KEY');
     this.request = requestBuilder('dishes');
+}
+function parseBody(response) {
+    return JSON.parse(response.body);
 }
 
 DishCtrl.prototype.search = function dishSearch(query) {
-    return this.request('search', query);
+    var self = this;
+    return self.request('search', query)
+        .then(parseBody)
+        .then(function(body) {
+            self.body = {
+                count: body.stats.count,
+                list: body.dishes
+            };
+            return Bluebird.join(
+                getExtremes.call(self, query, 'date'),
+                getExtremes.call(self, query, 'date', true),
+                getExtremes.call(self, query, 'popularity'),
+                function(oldest, newest, popular) {
+                    // console.log('oldest', _.keys(oldest.dishes));
+                    // self.body.oldest = oldest.dishes[0];
+                    // self.body.newest = newest.dishes[0];
+                    console.log(oldest);
+                    console.log(newest);
+                    console.log(popular);
+                    // self.body.mostPopular = popular.dishes[0];
+                    return self.body;
+                });
+        });
 };
+
+function getExtremes(query, sort_by, last) {
+    var query = _.clone(query);
+    query.sort_by = sort_by;
+    query.per_page = 1;
+    query.page = last ? this.body.count : 1;
+    return this.request('search', query)
+        .then(parseBody);
+}
 
 module.exports = DishCtrl;
--- server/api/dishes/index.js
+++ server/api/dishes/index.js
@@ -1,11 +1,14 @@
-var DishCtrl = require('./dish.controller.js'),
+var _ = require('lodash'),
+    DishCtrl = require('./dish.controller.js'),
     router = require('express').Router();
 
 router.get('/search', function(req, res, next) {
     var dish = new DishCtrl();
-    dish.search(req.query)
+    var query = _.clone(req.query);
+    query.sort_by = 'name';
+    query.per_page = 10;
+    dish.search(query)
         .then(function(payload) {
-            console.log(payload);
             res.status(200).json(payload);
         }).catch(next);
 });
